939a1528f001b4e614f198f4862c4dabc350157e
Felipe Cabeza <felipe.cabeza@gmfinancial.com>
2022-10-09 13:02:36 -0500

    getters and setters for Estudiante

--- src/personas/Estudiante.java
+++ src/personas/Estudiante.java
@@ -1,11 +1,13 @@
 package personas;
 
 import Persona;
+import gestion.Asignatura;
+
 import java.util.*;
 
 public class Estudiante extends Persona {
 
-	private ArrayList<Dictionary> asignaturasCursadas;
+	private ArrayList<Dictionary> asignaturasInscritas;
 	private float promedio;
 	private int semestre;
 	private String LineaEnfasis;
@@ -13,15 +15,54 @@ public class Estudiante extends Persona {
 	
 	@Override
 	public float calcularPromedio() {
-		
+
 	}
 	
 	public String calidadEstudiante() {
 		
 	}
 	
-	public Array[Asignatura] recomendarAsignaturas(){
+	public ArrayList<Asignatura> recomendarAsignaturas(){
 		
 	}
-	
+
+	public ArrayList<Dictionary> getAsignaturasInscritas() {
+		return asignaturasInscritas;
+	}
+
+	public void setAsignaturasInscritas(ArrayList<Dictionary> asignaturasInscritas) {
+		this.asignaturasInscritas = asignaturasInscritas;
+	}
+
+	public float getPromedio() {
+		return promedio;
+	}
+
+	public void setPromedio(float promedio) {
+		this.promedio = promedio;
+	}
+
+	public int getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(int semestre) {
+		this.semestre = semestre;
+	}
+
+	public String getLineaEnfasis() {
+		return LineaEnfasis;
+	}
+
+	public void setLineaEnfasis(String lineaEnfasis) {
+		LineaEnfasis = lineaEnfasis;
+	}
+
+	public ArrayList<Asignatura> getAsignaturasAprobadas() {
+		return asignaturasAprobadas;
+	}
+
+	public void setAsignaturasAprobadas(ArrayList<Asignatura> asignaturasAprobadas) {
+		this.asignaturasAprobadas = asignaturasAprobadas;
+	}
 }
