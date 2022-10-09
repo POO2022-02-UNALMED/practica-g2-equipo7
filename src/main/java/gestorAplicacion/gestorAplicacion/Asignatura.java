055be50eeff333c368761d404b1f7caecc29bc60
anvillaa <111092507+anvillaa@users.noreply.github.com>
2022-10-09 10:01:40 -0500

    1

---
+++ src/main/java/gestorAplicacion/gestorAplicacion/Asignatura.java
@@ -0,0 +1,57 @@
+package gestorAplicacion;
+package gestion;
+
+import java.util.*;
+
+public class Asignatura {
+	private int creditos;
+	private ArrayList<Profesor> profesor;
+	private ArrayList<Prerrequisitos> prerrequisitos;
+	private String lineaEnfasis;
+	private static Asignatura asignaturaPorEnfasis;
+	
+	public void displayProfesores() {
+		
+	}
+
+	public int getCreditos() {
+		return creditos;
+	}
+
+	public void setCreditos(int creditos) {
+		this.creditos = creditos;
+	}
+
+	public ArrayList<Profesor> getProfesor() {
+		return profesor;
+	}
+
+	public void setProfesor(ArrayList<Profesor> profesor) {
+		this.profesor = profesor;
+	}
+
+	public ArrayList<Prerrequisitos> getPrerrequisitos() {
+		return prerrequisitos;
+	}
+
+	public void setPrerrequisitos(ArrayList<Prerrequisitos> prerrequisitos) {
+		this.prerrequisitos = prerrequisitos;
+	}
+
+	public String getLineaEnfasis() {
+		return lineaEnfasis;
+	}
+
+	public void setLineaEnfasis(String lineaEnfasis) {
+		this.lineaEnfasis = lineaEnfasis;
+	}
+
+	public static Asignatura getAsignaturaPorEnfasis() {
+		return asignaturaPorEnfasis;
+	}
+
+	public static void setAsignaturaPorEnfasis(Asignatura asignaturaPorEnfasis) {
+		Asignatura.asignaturaPorEnfasis = asignaturaPorEnfasis;
+	}
+	
+}
---
+++ src/main/java/gestorAplicacion/gestorAplicacion/Persona.java
@@ -0,0 +1,36 @@
+package gestorAplicacion;
+import java.util.*;
+
+public abstract class Persona {
+	private  int documento;
+	private  String nombre;
+	private int edad;
+	
+	public abstract float calcularPromedio();
+
+	public int getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(int documento) {
+		this.documento = documento;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	
+}
